33588aa67fdf719fb03149e9129ed8d12a868eb7
hxb <hxb@winning.com.cn>
2022-11-23 23:58:09 +0800

    feat: add webpack config files.

---
+++ build/webpack.config.js
@@ -0,0 +1,7 @@
+const { merge } = require('webpack-merge')
+const baseConfig = require('./webpack.base.config.js')
+const devConfig = require('./webpack.dev.config.js')
+const proConfig = require('./webpack.pro.config.js')
+
+let config = process.NODE_ENV === 'development' ? devConfig : proConfig
+module.exports = merge(baseConfig, config)
